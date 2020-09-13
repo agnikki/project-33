99070df0ca91d00c6e5b4d6f33c82f99476b0373
agnikki <68161276+agnikki@users.noreply.github.com>
2020-09-13 14:28:44 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+
 var PLAY=1;
 var END=0;
 var gameState=PLAY;
@@ -11,7 +12,7 @@ var plinkos = [];
 var divisions=[];
 var divisionHeight=300;
 var score =0;
-var turn =0;
+var count =0;
 function setup() {
   createCanvas(800, 800);
   engine = Engine.create();
@@ -68,10 +69,10 @@ function draw() {
    }
    
  
-  for (var j = 0; j < particles.length; j++) {
+  //for (var j = 0; j < particles.length; j++) {
    
-     particles[j].display();
-   }
+     //particles[j].display();
+  // }
    for (var k = 0; k < divisions.length; k++) {
      
      divisions[k].display();
@@ -102,7 +103,7 @@ function draw() {
    text("GAME OVER",150,250);
  }
 
- if(gameState!==PLAY){
+
      if(particle!==null)
    {
      particle.display();
@@ -113,27 +114,14 @@ function draw() {
         score=score+500;
         particle=null;
         if(count>= 5) gameState =END;
+   
       }
-     }
-   }
-   if(particle!==null)
-   {
-     particle.display();
-
-     if (particle.body.position.y>760){
-
       if(particle.body.position.x>301 && particle.body.position.x<600){
         score=score+100;
         particle=null;
         if(count>= 5) gameState =END;
       }
-     }
-   }
-   if(particle!==null)
-   {
-     particle.display();
-
-     if (particle.body.position.y>760){
+  
 
       if(particle.body.position.x>601 && particle.body.position.x<800){
         score=score+300;
@@ -141,15 +129,12 @@ function draw() {
         if(count>= 5) gameState =END;
       }
      }
+    }
    }
- }
-}
+ 
 function keyPressed(){
-  if (keyCode===32){
-   if(gameState!==END)
-  {
+  if (keyCode===32 && gameState!==END){
     count++;
     particle=new Particle(mouseX,10,10);
   }
-}
 }
