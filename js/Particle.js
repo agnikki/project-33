61088547e2ad1bcfd3295513d4dccf8801a9c296
agnikki <68161276+agnikki@users.noreply.github.com>
2021-07-24 18:32:26 -0500

    Add files via upload

--- js/Particle.js
+++ js/Particle.js
@@ -6,7 +6,7 @@ class Particle {
         }
         this.r=r;
       
-        this.body = Bodies.circle(x, y, this.r,options);       
+        this.body = Bodies.circle(x, y, r,options);       
         this.color=color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
 
